8d62e1d8a9acc3781a93b952eb7779cc8a2efde0
bl4ckp4nther <divyanshus2396@gmail.com>
2017-07-13 16:41:56 +0530

    Authenticated Users

--- hello.py
+++ hello.py
@@ -1,10 +1,19 @@
-#lngae-173607
+#lngae-173610
 
 import webapp2
 
+from google.appengine.api import users
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        user = users.get_current_user()
+        if user:
+            logout_url=users.create_logout_url(self.request.uri)
+            self.response.write("welcomE you FUCKING LEGEND")
+            
+        else:
+            login_url = users.create_login_url(self.request.uri)
+            self.redirect(login_url)
 
 app = webapp2.WSGIApplication([
 ('/', MainHandler)
